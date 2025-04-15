1860fa59f15fcce74817dd927b6632a9b603aa9f
aviseu <alexandre@viseu.me>
2025-04-16 01:50:55 +0200

    cloud run service module now outputs service_account_email

--- modules/cloud_run_service/output.tf
+++ modules/cloud_run_service/output.tf
@@ -9,3 +9,7 @@ output "backend" {
 output "service_url" {
   value = google_cloud_run_v2_service.service.uri
 }
+
+output "service_account_email" {
+  value = google_service_account.service_account.email
+}
