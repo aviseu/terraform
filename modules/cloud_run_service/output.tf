a3ecf8e8c95d073ff8c8e68b71d5137c6274fc73
aviseu <alexandre@viseu.me>
2025-04-16 01:35:13 +0200

    cloud run services now output the service uri

--- modules/cloud_run_service/output.tf
+++ modules/cloud_run_service/output.tf
@@ -5,3 +5,7 @@ output "service_name" {
 output "backend" {
   value = google_compute_region_backend_service.backend.self_link
 }
+
+output "service_url" {
+  value = google_cloud_run_v2_service.service.uri
+}
